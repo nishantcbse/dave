60b189be8accadf1c75709d779fa7b9bbb846c9b
vikas ranjna <vikash_raz@yahoo.co.in>
2014-01-21 02:22:07 +0530

    validation not right

---
+++ pages/modals_payment.php
@@ -0,0 +1,23 @@
+<style>
+.modelbo{
+	height:32px !important;
+}</style>
+
+<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+	<h3>Conferm</h3>
+</div>
+<div class="modal-body">
+	<div class="tab-content">
+		<div class="tab-pane active" id="">
+			<p>Do you want to continue</p>
+		</div>
+	</div>
+<!--	<p>
+		<button class="btn green update modelbo">Update</button>
+	</p>
+--></div>
+<div class="modal-footer">
+	<button type="button" data-dismiss="modal" class="btn">No</button>
+	<button type="button" class="btn green update modelbo" onclick="redirect1()">Yes</a>
+</div>
