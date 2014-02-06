ba3a6d5ca41ed17ce01ca3b8e1b9ab995b8e1713
nishant <nishantcbse@gmail.com>
2014-02-06 22:46:42 +0530

    search

--- js/header.js
+++ js/header.js
@@ -1,9 +1,8 @@
 // JavaScript Document
 function getSearch(){
-        var abc = $('#searchText').val();
-var l = window.location;
-var base_url = l.protocol + "//" + l.host + "/" + l.pathname.split('/')[1];
-
-		alert(base_url);
-        window.location.href = url + '?alpha=' + abc;
+		var abc = $('#searchText').val();
+		var l = window.location;
+		var base_url = l.protocol + "//" + l.host + "/" + l.pathname.split('/')[1];
+		var searchUrl = base_url + '/search.php?search=' + abc;
+		window.location.replace(searchUrl);
 }
--- js/search.js
+++ js/search.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+$(document).ready(function(){
+	
+})
+
+function search_candidate(){
+    var search = $('#search_val').val();
+	$.get('classes/search.class.php',{search:search,searchtype:'candidate'}).done(function(data){
+		   $('#search_result').html(data); 
+		})
+}
