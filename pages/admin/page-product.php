7cc9a345ff3071142a4d5afe0739cd71021e35ef
vikas ranjan <vikash_raz@yahoo.co.in>
2014-02-09 05:40:49 +0530

    progole-image&video

---
+++ pages/admin/page-product.php
@@ -0,0 +1,71 @@
+            <div class="row-fluid">
+              <div class="span12">
+                <div class="span2">
+          <ul class="ver-inline-menu tabbable margin-bottom-10">
+            <li class="active"> <a data-toggle="tab" href="#home1"> <i class="icon-picture"></i> Home </a> <span class="after"></span></li>
+            <li ><a data-toggle="tab" href="#register1"><i class="icon-picture"></i> Register </a></li>
+            <li ><a data-toggle="tab" href="#login1"><i class="icon-picture"></i> Login </a></li>
+            <li ><a data-toggle="tab" href="#contactus1"><i class="icon-picture"></i> Contact </a></li>
+            <li ><a data-toggle="tab" href="#about1"><i class="icon-picture"></i> About </a></li>
+            <li ><a data-toggle="tab" href="#welcome1"><i class="icon-picture"></i> Welcome </a></li>
+            <li ><a data-toggle="tab" href="#accountsponser1"><i class="icon-picture"></i> Account Sponser </a></li>
+            <li ><a data-toggle="tab" href="#candidateinfo1"><i class="icon-picture"></i> Candidate Info </a></li>
+          </ul>
+                </div>
+                
+                <div class="span10">
+                  <div class="tab-content">
+                    <div id="home1" class="tab-pane active">
+                      <div style="height: auto;" id="home1" class="accordion collapse">
+                        <?php include_once('page-product-home.php'); ?>
+                      </div>
+                    </div>
+                    
+                    <div id="register1" class="tab-pane">
+                      <div style="height: auto;" id="register1" class="accordion collapse">
+                    <?php include_once('page-product-register.php'); ?>
+                    </div>
+                  </div>
+                    
+                    <div id="login1" class="tab-pane">
+                      <div style="height: auto;" id="login1" class="accordion collapse">
+                    <?php include_once('page-product-login.php'); ?>
+                    </div>
+                  </div>
+                    
+                    <div id="contactus1" class="tab-pane">
+                      <div style="height: auto;" id="contactu1" class="accordion collapse">
+                    <?php include_once('page-product-contactus.php'); ?>
+                    </div>
+                  </div>
+                    
+                    <div id="about1" class="tab-pane">
+                      <div style="height: auto;" id="about1" class="accordion collapse">
+                    <?php include_once('page-product-about.php'); ?>
+                    </div>
+                  </div>
+                    
+                    <div id="welcome1" class="tab-pane">
+                      <div style="height: auto;" id="welcome1" class="accordion collapse">
+                    <?php include_once('page-product-welcome.php'); ?>
+                    </div>
+                  </div>
+                    
+                    <div id="accountsponser1" class="tab-pane">
+                      <div style="height: auto;" id="accountsponser" class="accordion collapse">
+                    <?php include_once('page-product-accountsponser.php'); ?>
+                    </div>
+                  </div>
+                    
+                    <div id="candidateinfo1" class="tab-pane">
+                      <div style="height: auto;" id="candidateinfo1" class="accordion collapse">
+                    <?php include_once('page-product-candidateinfo.php'); ?>
+                    </div>
+                  </div>
+                  
+                   </div>
+                <!--end span9--> 
+              </div>
+               </div>
+            </div>
+            
