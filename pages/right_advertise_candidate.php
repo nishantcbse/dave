9b9366f6fa61c345c8b8324afafd28ca7120cd67
nishant <nishantcbse@gmail.com>
2014-02-15 10:53:58 +0530

    register

---
+++ pages/right_advertise_candidate.php
@@ -0,0 +1,43 @@
+
+<style>
+.pricetag{
+ padding:10px !important;
+}
+.advertiseregister span4{
+   margin-left:0px !important;
+   
+}
+.row-fluid .advertise {
+    margin-bottom: 10px;
+    margin-left: 0px !important;
+    margin-right: 10px !important;
+}
+</style>
+<?php
+include_once('classes/function_product.php');
+?>
+<div class="span8 front-carousel pull-right">
+          
+       <div class="row-fluid advertiseregister">
+                <?php echo display_candidate_product(); ?>
+                
+        </div>
+                  
+            
+
+
+				
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
